082f40bb37de993a147ec73518463fc7f621707f
chinyunchen <chenclaire17@gmail.com>
2019-02-27 00:55:38 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const css = document.querySelector("h3");
+
+const colorLeft = document.getElementById("color-left"); 
+const colorPickerLeft = document.getElementById("color-picker-left");
+
+const colorRight = document.getElementById("color-right");
+const colorPickerRight = document.getElementById("color-picker-right");
+// colorPickerRight.style.backgroundColor = colorRight.value;
+
+const body = document.getElementById("gradient");
+
+function pickColor(){
+	colorPickerRight.style.backgroundColor = colorRight.value;
+	colorPickerLeft.style.backgroundColor = colorLeft.value;
+	body.style.background = 
+	"linear-gradient(to left,"
+	+ colorRight.value
+	+ "," 
+	+ colorLeft.value
+	+ ")" ;
+
+	css.textContent = body.style.background + ";" ;
+}
+
+
+colorLeft.addEventListener("input", pickColor);
+
+colorRight.addEventListener("input", pickColor);
